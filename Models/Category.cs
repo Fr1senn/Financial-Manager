319a1571377d2e91215e4e39b9a4479293acdfff
Vlad Bomba <bombavlad1@gmail.com>
2024-08-01 10:12:45 +0300

    models that represent database entities are created

---
+++ Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace financial_manager.Models;
+
+public partial class Category
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+
+    public int UserId { get; set; }
+
+    public virtual ICollection<Transaction> Transactions { get; set; } = new List<Transaction>();
+
+    public virtual User User { get; set; } = null!;
+}
---
+++ Models/Transaction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace financial_manager.Models;
+
+public partial class Transaction
+{
+    public int Id { get; set; }
+
+    public int UserId { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public int? CategoryId { get; set; }
+
+    public decimal Significance { get; set; }
+
+    public DateTime? ExpenseDate { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public virtual Category? Category { get; set; }
+
+    public virtual User User { get; set; } = null!;
+}
