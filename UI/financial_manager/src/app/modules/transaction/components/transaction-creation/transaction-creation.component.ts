4b11cf9ee675befec09c7265f94b37d67aaea599
Vlad Bomba <bombavlad1@gmail.com>
2024-08-08 11:19:43 +0300

    a component for adding a transaction was created

---
+++ UI/financial_manager/src/app/modules/transaction/components/transaction-creation/transaction-creation.component.ts
@@ -0,0 +1,8 @@
+import { Component, inject, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-transaction-creation',
+  templateUrl: './transaction-creation.component.html',
+  styleUrl: './transaction-creation.component.css',
+})
+export class TransactionCreationComponent implements OnInit {
+}
