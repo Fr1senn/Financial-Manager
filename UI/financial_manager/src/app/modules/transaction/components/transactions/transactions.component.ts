8252d3750b8372ec276d73364db060ac4a3575b1
Vlad Bomba <bombavlad1@gmail.com>
2024-08-08 11:34:26 +0300

    implemented opening of the transaction creation component using a modal window

--- UI/financial_manager/src/app/modules/transaction/components/transactions/transactions.component.ts
+++ UI/financial_manager/src/app/modules/transaction/components/transactions/transactions.component.ts
@@ -1,5 +1,8 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Transaction } from '../../../../models/transaction';
+import { MatDialog } from '@angular/material/dialog';
+import { TransactionCreationComponent } from '../transaction-creation/transaction-creation.component';
+import { TransactionService } from '../../services/transaction.service';
 
 @Component({
   selector: 'app-transactions',
@@ -9,6 +12,7 @@ import { Transaction } from '../../../../models/transaction';
 export class TransactionsComponent implements OnInit {
   public transactions: Transaction[] = [];
 
+  private readonly dialog: MatDialog = inject(MatDialog);
   private readonly transactionService: TransactionService =
     inject(TransactionService);
 
@@ -16,4 +20,7 @@ export class TransactionsComponent implements OnInit {
     this.transactions = this.transactionService.getTransactions();
   }
 
+  public createTransaction(): void {
+    this.dialog.open(TransactionCreationComponent);
+  }
 }
