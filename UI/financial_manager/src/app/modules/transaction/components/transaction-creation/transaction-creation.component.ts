1c53ef64890d58b80746fd153cf654b8164fd805
Vlad Bomba <bombavlad1@gmail.com>
2024-08-08 11:30:56 +0300

    transaction creation in the component is implemented

--- UI/financial_manager/src/app/modules/transaction/components/transaction-creation/transaction-creation.component.ts
+++ UI/financial_manager/src/app/modules/transaction/components/transaction-creation/transaction-creation.component.ts
@@ -1,7 +1,10 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { TransactionCategoryService } from '../../services/transaction-category.service';
+import { Transaction } from '../../../../models/transaction';
+import { TransactionService } from '../../services/transaction.service';
 import { TransactionCreationFormValidatorService } from '../../utilities/transaction-creation-form-validator.service';
 import { FormGroup } from '@angular/forms';
+
 @Component({
   selector: 'app-transaction-creation',
   templateUrl: './transaction-creation.component.html',
@@ -9,11 +12,31 @@ import { FormGroup } from '@angular/forms';
 })
 export class TransactionCreationComponent implements OnInit {
   public transactionCategories: Category[] = [];
+  public transaction: Transaction = {
+    title: '',
+    significance: 1,
+    transactionType: '',
+    expenseDate: undefined,
+    createdAt: new Date(),
+  };
   public transactionCreationForm: FormGroup | undefined;
   private readonly transactionCategoryService: TransactionCategoryService =
     inject(TransactionCategoryService);
+  private readonly transactionService: TransactionService =
+    inject(TransactionService);
   private readonly transactionCreationFormValidatorService: TransactionCreationFormValidatorService =
     inject(TransactionCreationFormValidatorService);
+
+  public createTransaction() {
+    if (this.transaction.expenseDate === undefined) {
+      this.transaction.expenseDate = new Date();
+    } else {
+      this.transaction.expenseDate = new Date(this.transaction.expenseDate);
+    }
+    this.transactionService.createTransaction(this.transaction);
+    this.dialogRef.close();
+  }
+
   public ngOnInit(): void {
     this.getTransactionCategories();
     this.transactionCreationForm =
