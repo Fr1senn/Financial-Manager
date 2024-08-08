ec651d6e49eb8ccd8d0248c6b0ef7f24bdb8254b
Vlad Bomba <bombavlad1@gmail.com>
2024-08-08 11:23:23 +0300

    field validation service is implemented in the component

--- UI/financial_manager/src/app/modules/transaction/components/transaction-creation/transaction-creation.component.ts
+++ UI/financial_manager/src/app/modules/transaction/components/transaction-creation/transaction-creation.component.ts
@@ -1,8 +1,17 @@
 import { Component, inject, OnInit } from '@angular/core';
+import { TransactionCreationFormValidatorService } from '../../utilities/transaction-creation-form-validator.service';
+import { FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-transaction-creation',
   templateUrl: './transaction-creation.component.html',
   styleUrl: './transaction-creation.component.css',
 })
 export class TransactionCreationComponent implements OnInit {
+  public transactionCreationForm: FormGroup | undefined;
+  private readonly transactionCreationFormValidatorService: TransactionCreationFormValidatorService =
+    inject(TransactionCreationFormValidatorService);
+  public ngOnInit(): void {
+    this.transactionCreationForm =
+      this.transactionCreationFormValidatorService.createForm();
+  }
 }
