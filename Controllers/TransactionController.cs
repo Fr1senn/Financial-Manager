c3296e93048df18b107cc5fec646027743405f6d
Vlad Bomba <bombavlad1@gmail.com>
2024-08-09 10:55:03 +0300

    a controller for interaction with transactions was created

---
+++ Controllers/TransactionController.cs
@@ -0,0 +1,13 @@
+﻿using financial_manager.Repositories;
+using financial_manager.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace financial_manager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+    }
+}
