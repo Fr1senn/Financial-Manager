13e4c6a0b36026933fb0d3600ebdfabeec5e7b3d
Vlad Bomba <bombavlad1@gmail.com>
2024-08-09 11:33:22 +0300

    a type is created that returns the result of the operation

---
+++ Models/OperationResult.cs
@@ -0,0 +1,14 @@
+﻿namespace financial_manager.Models
+{
+    public class OperationResult
+    {
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+
+        public OperationResult(bool success, string? message = null)
+        {
+            Success = success;
+            Message = message;
+        }
+    }
+}
