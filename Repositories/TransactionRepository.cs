4a6d06a1e743964b9402c7487d65fe9c81d31304
Vlad Bomba <bombavlad1@gmail.com>
2024-08-09 10:49:01 +0300

    a repository for transaction data manipulation has been created

---
+++ Repositories/TransactionRepository.cs
@@ -0,0 +1,11 @@
+﻿using financial_manager.Entities;
+using financial_manager.Models;
+using financial_manager.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace financial_manager.Repositories
+{
+    public class TransactionRepository : ITransactionRepository
+    {
+    }
+}
