b2492d39ed060b0d12d64cc07ad9fc3b5d889fe2
Vlad Bomba <bombavlad1@gmail.com>
2024-08-09 10:56:26 +0300

    transaction receipt endpoint is created

--- Controllers/TransactionController.cs
+++ Controllers/TransactionController.cs
@@ -9,5 +9,18 @@ namespace financial_manager.Controllers
     [ApiController]
     public class TransactionController : ControllerBase
     {
+        private readonly ITransactionRepository _transactionRepository;
+
+        public TransactionController(ITransactionRepository transactionRepository)
+        {
+            _transactionRepository = transactionRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetTransactionsAsync([FromQuery] int packSize = 10, [FromQuery] int pageNumber = 0)
+        {
+            return Ok(await _transactionRepository.GetTransactionsAsync(packSize, pageNumber));
+            //return Ok(10);
+        }
     }
 }
