ec9166cf91bf73149f333a91e823aa20910c48b8
Vlad Bomba <bombavlad1@gmail.com>
2024-08-10 16:36:05 +0300

    added a field that stores the http response code

--- Models/OperationResult.cs
+++ Models/OperationResult.cs
@@ -1,13 +1,18 @@
-﻿namespace financial_manager.Models
+﻿using financial_manager.Models.Enums;
+
+namespace financial_manager.Models
 {
     public class OperationResult
     {
         public bool Success { get; set; }
+        public HttpResponseCode HttpResponseCode { get; set; }
         public string? Message { get; set; }
 
         public OperationResult(bool success, string? message = null)
+        public OperationResult(bool isSuccess, HttpResponseCode httpResponseCode, string? message = null)
         {
             Success = success;
+            HttpResponseCode = httpResponseCode;
             Message = message;
         }
     }
