9eec3671afcab34729edcde28aeae95615c573ac
Vlad Bomba <bombavlad1@gmail.com>
2024-08-12 10:18:14 +0300

    migration was created due to a change in the structure of the database table

---
+++ Migrations/20240812071226_TransactionExpenseDateFieldNotNull.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace financial_manager.Migrations
+{
+    /// <inheritdoc />
+    public partial class TransactionExpenseDateFieldNotNull : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "expense_date",
+                table: "transactions",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldNullable: true,
+                oldDefaultValueSql: "CURRENT_TIMESTAMP");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "expense_date",
+                table: "transactions",
+                type: "timestamp without time zone",
+                nullable: true,
+                defaultValueSql: "CURRENT_TIMESTAMP",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "CURRENT_TIMESTAMP");
+        }
+    }
+}
