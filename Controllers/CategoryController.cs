4c88d5db91972675570b97ac07221e3af0cbf282
Vlad Bomba <bombavlad1@gmail.com>
2024-08-12 12:16:20 +0300

    the transaction category creation endpoint is implemented

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,3 +1,6 @@
+﻿using financial_manager.Models;
+using financial_manager.Models.Enums;
+using financial_manager.Repositories.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,5 +10,28 @@ namespace financial_manager.Controllers
     [ApiController]
     public class CategoryController : ControllerBase
     {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCategoryAsync([FromBody] Category category)
+        {
+            try
+            {
+                await _categoryRepository.CreateCategoryAsync(category);
+                return Ok(new OperationResult(true, HttpResponseCode.NoContent));
+            }
+            catch (NullReferenceException ex)
+            {
+                return BadRequest(new OperationResult(false, HttpResponseCode.BadRequest, ex.Message));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new OperationResult(false, HttpResponseCode.BadRequest, ex.Message));
+            }
+        }
     }
 }
