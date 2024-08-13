a84ac9a52c39d8616551145f2be7f560f560620b
Vlad Bomba <bombavlad1@gmail.com>
2024-08-13 11:46:53 +0300

    implemented method of receiving transactions by users from API

---
+++ UI/financial_manager/src/app/modules/transaction/services/interfaces/transaction.interface.ts
@@ -0,0 +1,11 @@
+import { Observable } from 'rxjs';
+import { OperationResult } from '../../../../models/operation-result';
+import { Transaction } from '../../../../models/transaction';
+import { HttpResponseCode } from '../../../../models/enums/http-response-code';
+
+export interface ITransactionService {
+  getTransactions(
+    packSize: number,
+    pageNumber: number
+  ): Observable<OperationResult<Transaction>>;
+}
--- UI/financial_manager/src/app/modules/transaction/services/transaction.service.ts
+++ UI/financial_manager/src/app/modules/transaction/services/transaction.service.ts
@@ -1,13 +1,28 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Transaction } from '../../../models/transaction';
-
+import { enviroment } from '../../../enviroment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { OperationResult } from '../../../models/operation-result';
+import { ITransactionService } from './interfaces/transaction.interface';
+import { HttpResponseCode } from '../../../models/enums/http-response-code';
 
 @Injectable({
   providedIn: 'root',
 })
-export class TransactionService {
-  public getTransactions() {
-    return transactions;
+export class TransactionService implements ITransactionService {
+  private readonly baseApiUrl: string = enviroment.baseApiUrl;
+  private readonly httpClient: HttpClient = inject(HttpClient);
+
+  public getTransactions(
+    packSize: number = 10,
+    pageNumber: number = 0
+  ): Observable<OperationResult<Transaction>> {
+    return this.httpClient.get<OperationResult<Transaction>>(
+      `${this.baseApiUrl}/Transaction`,
+      { params: { packSize: packSize, pageNumber: pageNumber } }
+    );
+  }
   }
 
   public createTransaction(transaction: Transaction): void {
