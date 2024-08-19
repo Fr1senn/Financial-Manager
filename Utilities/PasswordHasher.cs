4f962e0a23babc1fe1ec4639b7c5d2987ee1c050
Vlad Bomba <bombavlad1@gmail.com>
2024-08-19 10:57:27 +0300

    a method that hashes the raw password is implemented

--- Utilities/PasswordHasher.cs
+++ Utilities/PasswordHasher.cs
@@ -6,5 +6,19 @@ namespace financial_manager.Utilities
 {
     public class PasswordHasher : IPasswordHasher
     {
+        private const int SaltSize = 16;
+        private const int HashSize = 32;
+        private const int Iterations = 10000;
+        public string HashPassword(string rawPassword, byte[] salt)
+        {
+            var hashedPassword = KeyDerivation.Pbkdf2(
+                password: rawPassword,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: Iterations,
+                numBytesRequested: HashSize);
+
+            return Convert.ToBase64String(hashedPassword);
+        }
     }
 }
