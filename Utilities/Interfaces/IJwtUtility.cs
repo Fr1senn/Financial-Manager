66b8d6c24a8ba5180a425495c4499ddf2ada55b5
Vlad Bomba <bombavlad1@gmail.com>
2024-08-21 17:27:30 +0300

    method that generates JWT is implemented

---
+++ Utilities/Interfaces/IJwtUtility.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Claims;
+
+namespace financial_manager.Utilities.Interfaces
+{
+    public interface IJwtUtility
+    {
+        string GenerateAccessToken(IEnumerable<Claim> claims);
+    }
+}
---
+++ Utilities/JwtUtility.cs
@@ -0,0 +1,36 @@
+﻿using financial_manager.Utilities.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace financial_manager.Utilities
+{
+    public class JwtUtility : IJwtUtility
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtUtility(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateAccessToken(IEnumerable<Claim> claims)
+        {
+            var jwtSettings = _configuration.GetSection("Jwt");
+            var key = Encoding.ASCII.GetBytes(jwtSettings["Key"]!);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddMinutes(int.Parse(jwtSettings["AccessTokenExpirationMinutes"]!)),
+                Issuer = jwtSettings["Issuer"],
+                Audience = jwtSettings["Audience"],
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
