301e6e27266857e5165977dc5ae662cb39300a0d
Vlad Bomba <bombavlad1@gmail.com>
2024-08-22 17:20:54 +0300

    controller has been implemented

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,15 @@
+﻿using financial_manager.Models;
+using financial_manager.Models.Enums;
+using financial_manager.Repositories.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace financial_manager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+    }
+}
