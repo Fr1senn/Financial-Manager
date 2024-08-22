2665eac3c539365beb72e6ee30ce244e0f6ed085
Vlad Bomba <bombavlad1@gmail.com>
2024-08-22 12:57:25 +0300

    a model is created that represents the database entity

---
+++ Models/Token.cs
@@ -0,0 +1,11 @@
+﻿namespace financial_manager.Models
+{
+    public class Token
+    {
+        public int Id { get; set; }
+        public string RefreshToken { get; set; } = null!;
+        public bool IsRevoked { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public User? User { get; set; }
+    }
+}
