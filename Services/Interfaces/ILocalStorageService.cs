4afe48e5d48c6f3581960e71f31e87ae2c90e51b
Vlad Bomba <bombavlad1@gmail.com>
2024-08-22 12:24:58 +0300

    service for working with Redis is implemented

---
+++ Services/Interfaces/ILocalStorageService.cs
@@ -0,0 +1,8 @@
+﻿namespace financial_manager.Services.Interfaces
+{
+    public interface ILocalStorageService
+    {
+        Task SetAsync(string key, string value, TimeSpan expiry);
+        Task<string?> GetAsync(string key);
+    }
+}
