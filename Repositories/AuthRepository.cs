6470782621809458969a26c55248d107ec83f3b1
Vlad Bomba <bombavlad1@gmail.com>
2024-08-22 12:47:53 +0300

    a repository for user authorization and authentication is implemented

---
+++ Repositories/AuthRepository.cs
@@ -0,0 +1,13 @@
+﻿using financial_manager.Entities;
+using financial_manager.Models;
+using financial_manager.Repositories.Interfaces;
+using financial_manager.Utilities.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace financial_manager.Repositories
+{
+    public class AuthRepository : IAuthRepository
+    {
+    }
+}
---
+++ Repositories/Interfaces/IAuthRepository.cs
@@ -0,0 +1,8 @@
+﻿using financial_manager.Models;
+
+namespace financial_manager.Repositories.Interfaces
+{
+    public interface IAuthRepository
+    {
+    }
+}
