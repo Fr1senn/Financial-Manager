7ae74a563663237eda5c3f7e059d1e2cd5452060
Vlad Bomba <bombavlad1@gmail.com>
2024-08-22 12:32:00 +0300

    a model for obtaining authorization data was created

---
+++ Models/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace financial_manager.Models
+{
+    public class LoginModel
+    {
+        public string Email { get; set; } = string.Empty;
+        public string RawPassword { get; set; } = string.Empty;
+    }
+}
